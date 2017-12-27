f008404f3932fa7036abecc48cc4a55c89564faa
ashwinjikumar <33451829+ashwinjikumar@users.noreply.github.com>
2017-12-27 17:01:35 +0100

    Add files via upload

--- ex9p4.py
+++ ex9p4.py
@@ -1,32 +1,19 @@
 name = input("Enter file name: ")
 if len(name) < 1 : name = "mbox-short.txt"
-handle = open(name,'r')
-count = 0
-o_dic=dict()
-dum=None
+handle = open(name)
+counts = dict()
 for line in handle:
-    line=line.rstrip()
-    wds=line.split()
-    if len(wds)<3 or wds[0]!='From':
-        continue
-    #print(wds[1])
-    if dum==wds[1]:
-        dum=wds[1]
-        o_dic[dum]=o_dic[dum]+1
-    else :
-        dum=wds[1]
-        o_dic[dum]=1
-print(o_dic)
-bigcount=None
-bigword=None
-for word,count in o_dic.items():
-    #for word in wds
-    #o_dic[word]=o_dic.get(word,0)+1
-    #print(o_dic)
-    if bigcount is None or count>bigcount:
-        bigword=word
-        bigcount=count
-print("now final output")
-print(bigword,bigcount)
+    wds = line.split()
+    if len(wds) < 2 : continue
+    if wds[0] != "From" : continue
+    email = wds[1]
+    counts[email] = counts.get(email,0) + 1
+bigcount = None
+bigname = None
+for name,count in counts.items():
+    if bigname is None or count > bigcount:
+        bigname = name
+        bigcount = count
+print bigname, bigcount
 
-#print(o_dic)
+#print(counts)
